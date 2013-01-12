e5083d1098d5224709a4ed0919cdfe4a596bcc64
Justin Kronz <jkronz@gmail.com>
2013-01-12 09:30:58 -0500

    First commit, rails new.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ArtisanalPlaylist::Application.config.secret_token = 'bfc9e12fadf993c4747801089bf9301a1c405ffe809a5fa1fbd2eff76815fa949cf9c77b69923208d54cebb4020d97c14ed13d1ba13fd1f5c36ad52c6d66d381'
