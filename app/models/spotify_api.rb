a305a101dbde3644664f7bc8e7bdac0c7fffb557
Justin Kronz <jkronz@gmail.com>
2013-02-02 10:16:06 -0500

    Spotify track search.

--- app/models/spotify_api.rb
+++ app/models/spotify_api.rb
@@ -2,15 +2,9 @@ require 'typhoeus'
 require 'uri'
 class SpotifyApi
   class << self
-    def find_track(query)
+    def find_tracks(query, how_many=5)
       url = build_url(query)
-      parse_track(Typhoeus.get(url))
-    end
-
-    def echo_nest_to_spotify(echo_nest_result)
-      echo_nest_song = echo_nest_result['songs'].first
-      spotify_query = "#{echo_nest_song['artist_name']} #{echo_nest_song['title']}"
-      find_track(spotify_query)
+      parse_tracks(Typhoeus.get(url), how_many)
     end
 
     def build_url(query)
@@ -31,22 +25,27 @@ class SpotifyApi
       query_string
     end
 
-    def parse_track(response)
-      result = {}
-      #begin
+    def parse_tracks(response, how_many)
+      results = []
+      begin
         resp = JSON.parse(response.body)
         tracks = resp['tracks']
         return nil if tracks.length == 0
-        track = tracks.first
-        artists_array = track['artists'].collect { |art| art['name'] }
-        result[:artist] = artists_array.join(', ')
-        result[:length] = track['length'] * 1000
-        result[:uri] = track['href']
-        result[:name] = track['name']
-      #rescue
-      #  result = self.handle_api_error("There was a problem accessing the Spotify API.")
-      #end
-      result
+        cnt = [tracks.length, how_many].min - 1
+        tracks = tracks[0..cnt]
+        results = tracks.map do |track_item|
+          result = {}
+          artists_array = track_item['artists'].collect { |art| art['name'] }
+          result[:artist] = artists_array.join(', ')
+          result[:length] = track_item['length'] * 1000
+          result[:uri] = track_item['href']
+          result[:name] = track_item['name']
+          result
+        end
+      rescue
+        results = [] #we hit some exception. close the blinds & pretend there were no results.
+      end
+      results
     end
 
     def handle_api_error(message)
