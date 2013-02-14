7e48e63fcdfba08b50f0a78e9d6fcd682cf6a2e1
Justin Kronz <jkronz@gmail.com>
2013-02-14 13:43:01 -0500

    make sure dependencies are precompiled.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,7 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
+//= require dependencies
 //= require websocket_rails/main
 //= require init
 //= require models/track
